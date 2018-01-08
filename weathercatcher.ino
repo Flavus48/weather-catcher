bcbe18883e55c7e7dc8d0b5cae6377860707f291
Flavus48 <34524117+Flavus48@users.noreply.github.com>
2018-01-08 21:22:37 +0100

    First try for infrared remote control of lcd display

--- weathercatcher.ino
+++ weathercatcher.ino
@@ -1,3 +1,5 @@
+#include <IRremote.h>
+#include <LiquidCrystal.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
@@ -7,23 +9,80 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int RECV_PIN = 10;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+//int relayPin = 6; // Pin for Relay Control
+
 void setup() {
   // put your setup code here, to run once:
-  
-  Serial.begin(9600);
+
+  Serial.begin(9600); //Set Serial Baud Rate
+
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the IR-receiver
+  Serial.println("Enabled IRin");
+
   Serial.println("Humidity (%)\tTemperature (°C)");
-  dht.begin();
+  dht.begin(); //Configure Temp and Hum Sensor
+
+  //pinMode(8, OUTPUT); //Configure Pin of Relay as Output
+  
+
+  lcd.begin(16, 2); //Configure LCD Display with 16 Columns and 2 Rows
+  lcd.print("Measuring...");
+
+  delay(2000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  delay(4000);
-  
+
+  lcd.clear();
+  lcd.noDisplay();
+  delay(1000);
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
+  
+  long i = 0;
+  if (irrecv.decode(&results)) {   //irrecv.decode(&results) is 1 for pressed button, 0 for no button pressed
+    i = long(results.value);
+    Serial.println("i:");
+    Serial.println(results.value);
+    irrecv.resume();
+  }
+
+  while (i == 16761405) {
+    lcd.display();
+    humidity = dht.readHumidity();
+    temperature = dht.readTemperature();
+    lcd.clear();
+    lcd.print("Temp C");
+    lcd.setCursor(9, 0);
+    lcd.print("Hum %");
+    lcd.setCursor(0, 1);
+    lcd.print(temperature);
+    lcd.setCursor(9, 1);
+    lcd.print(humidity);
+    delay(4000);
+    if (irrecv.decode(&results)) {
+      Serial.println("i:");
+      i = long(results.value);
+      irrecv.resume();
+      lcd.noDisplay();
+      break;
+    }
+  }
 
   Serial.print(humidity);
   Serial.print("\t\t");
   Serial.println(temperature);
+
+// alle 10 min Wert in EEPROM speichern
+  
+
 }
