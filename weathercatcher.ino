da4a8c0a85ba4cbbb4b45e87f414919ff47ac00e
Flavus48 <34524117+Flavus48@users.noreply.github.com>
2018-01-08 21:35:08 +0100

    Improved IR control of LCD

--- weathercatcher.ino
+++ weathercatcher.ino
@@ -31,58 +31,48 @@ void setup() {
   dht.begin(); //Configure Temp and Hum Sensor
 
   //pinMode(8, OUTPUT); //Configure Pin of Relay as Output
-  
+
 
   lcd.begin(16, 2); //Configure LCD Display with 16 Columns and 2 Rows
   lcd.print("Measuring...");
-
   delay(2000);
+  lcd.noDisplay();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  lcd.clear();
-  lcd.noDisplay();
-  delay(1000);
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
-  
+  lcd.clear();
+  lcd.print("Temp C");
+  lcd.setCursor(9, 0);
+  lcd.print("Hum %");
+  lcd.setCursor(0, 1);
+  lcd.print(temperature);
+  lcd.setCursor(9, 1);
+  lcd.print(humidity);
+  delay(1000);
+
   long i = 0;
   if (irrecv.decode(&results)) {   //irrecv.decode(&results) is 1 for pressed button, 0 for no button pressed
     i = long(results.value);
-    Serial.println("i:");
-    Serial.println(results.value);
-    irrecv.resume();
-  }
-
-  while (i == 16761405) {
-    lcd.display();
-    humidity = dht.readHumidity();
-    temperature = dht.readTemperature();
-    lcd.clear();
-    lcd.print("Temp C");
-    lcd.setCursor(9, 0);
-    lcd.print("Hum %");
-    lcd.setCursor(0, 1);
-    lcd.print(temperature);
-    lcd.setCursor(9, 1);
-    lcd.print(humidity);
-    delay(4000);
-    if (irrecv.decode(&results)) {
-      Serial.println("i:");
-      i = long(results.value);
-      irrecv.resume();
-      lcd.noDisplay();
-      break;
+    if (i == 16761405) {
+      lcd.display();
     }
+    if (i != 16761405) {
+      lcd.noDisplay();
+    }
+    //Serial.println("i:");
+    //Serial.println(results.value);
+    irrecv.resume();
   }
 
   Serial.print(humidity);
   Serial.print("\t\t");
   Serial.println(temperature);
 
-// alle 10 min Wert in EEPROM speichern
-  
+  // alle 10 min Wert in EEPROM speichern
+
 
 }
